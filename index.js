fe599a5b3234bd146dfb4b2416c2818f907782ed
pedro-ed-correia <104008583+pedro-ed-correia@users.noreply.github.com>
2025-11-18 21:53:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var buttonsList = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttonsList.length; i++) {
+    buttonsList[i].addEventListener("click", function () {
+        playdrum(this.innerHTML);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    playdrum(event.key);
+});
+
+function playdrum(key) { 
+    switch (key) {
+        case "w":
+            new Audio("./sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("./sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("./sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("./sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("./sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("./sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("./sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log("Unknown key:", key);
+    }
+}
